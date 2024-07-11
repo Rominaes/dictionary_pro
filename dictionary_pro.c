a5afba896fc82d72289b5ac147e4f3996e9f69de
romina esfandani <romina.esfandani1384@gmail.com>
2024-07-11 21:32:13 +0330

    commit_do

--- dictionary_pro.c
+++ dictionary_pro.c
@@ -3,11 +3,66 @@
 #include <string.h>
 #include <ctype.h>
 
+#define MAX_SIZE 100
+
+struct User {
+    char username[MAX_SIZE];
+    char password[MAX_SIZE];
+    char firstname[MAX_SIZE];
+    char lastname[MAX_SIZE];
+    char phone[MAX_SIZE];
+    char email[MAX_SIZE];
+};
+
+
+
 int main()
 {
-
+    int choice = 0;
+    struct User *login = NULL;
+    while (choice != 4)
+    {
+        printf("\nMain Menu:\n");
+        printf("1. Sign Up\n");
+        printf("2. Sign In\n");
+        printf("3. Forgot Password\n");
+        printf("4. Exit\n");
+        printf("Choose an option: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            signUp();
+            break;
+        case 2:
+            signIn();
+            if (login != NULL)
+            {
+                if (strcmp(login->username, "Admin") == 0)
+                {
+                    adminMenu();
+                }
+                else
+                {
+                    userMenu();
+                }
+            }
+            break;
+        case 3:
+            forgotPassword();
+            break;
+        case 4:
+            printf("Exiting the program...\n");
+            break;
+        default:
+            printf("Invalid choice, Please try again.\n");
+        }
+    }
+    Save_words_in_file();
     return 0;
 }
+
+
 
 
 
